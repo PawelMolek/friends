d4de432554f1acc0eb448447e303ca64395f15a3
PawelMolek <pawel.molek@gmail.com>
2022-03-17 18:48:28 +0100

    Signing up works correctly, without error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,4 +310,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   #
+  # line below guarantee signing up without error
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 end
